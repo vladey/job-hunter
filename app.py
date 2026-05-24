83e66a9247c3222cf690d1e72cca598adc197791
vladey <vladi.rusev@gmail.com>
2026-05-24 09:26:20 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,14 +3,23 @@ from email_sender import send_email
 import schedule
 import time
 
+print("APP STARTED")
+
+
 def run():
 
-    jobs=search_jobs()
+    print("RUNNING SEARCH")
+
+    jobs = search_jobs()
+
+    print("FOUND JOBS:", len(jobs))
 
     if jobs:
         send_email(jobs)
+        print("EMAIL SENT")
 
     print("Done")
+
 
 schedule.every().day.at("08:00").do(run)
 schedule.every().day.at("15:00").do(run)
